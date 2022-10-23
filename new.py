4b2715d15f1f574f2032073e304b8993b555e768
AbhiGithub101 <88555629+AbhiGithub101@users.noreply.github.com>
2022-10-23 12:53:16 +0530

    Update new.py

--- new.py
+++ new.py
@@ -7,8 +7,9 @@ st.text('''In this project, we are going to analyze 12 months of data of sales.
 We will learn how to clean, manage and analyze the dataset to 
 find some meaningful information.''')
 with st.sidebar:
-    categorical = st.selectbox('Categorical', options=list(df.columns))
-    numerical = st.selectbox('Numerical', options=list(df.columns))
+    click4 = st.multiselect('Show The Data', options=list(df.columns))
+    categorical = st.selectbox('Categorical', options=['Select']+list(df.columns))
+    numerical = st.selectbox('Numerical', options=['Select']+list(df.columns))
     col1, col2, col3 = st.columns([1, 1, 1])
     with col1:
         click_sum = st.button('Sum')
@@ -18,27 +19,32 @@ with st.sidebar:
         pass
     click3 = st.slider('Top Products Sold Together',0,5)
 
-if (click_sum):
-    st.dataframe(df.groupby(categorical)[numerical].sum())
-    st.bar_chart(df.groupby(categorical)[numerical].sum())
-elif(click_count):
-    st.dataframe(df.groupby(categorical)[numerical].count())
-    st.bar_chart(df.groupby(categorical)[numerical].count())
 
-if(click3>0):
-    all_data = df.dropna(how='all')
-    df = all_data[all_data['Order ID'].duplicated(keep=False)]
+try:
+    if (click_sum):
+        st.dataframe(df.groupby(categorical)[numerical].sum())
+        st.bar_chart(df.groupby(categorical)[numerical].sum())
+    elif(click_count):
+        st.dataframe(df.groupby(categorical)[numerical].count())
+        st.bar_chart(df.groupby(categorical)[numerical].count())
+    elif(click3>0):
+        all_data = df.dropna(how='all')
+        df = all_data[all_data['Order ID'].duplicated(keep=False)]
 
-    from itertools import combinations
-    from collections import Counter
+        from itertools import combinations
+        from collections import Counter
 
-    count = Counter()
-    row_list = []
-    df['Grouped'] = df.groupby('Order ID')['Product'].transform(lambda x : ','.join(x))
-    df = df[['Order ID','Grouped']]
-    for row in df['Grouped']:
-        row_list = row.split(',')
-        count.update(Counter(combinations(row_list, click3))) #update method will update results
-                                                         #2 is the combination of two products
+        count = Counter()
+        row_list = []
+        df['Grouped'] = df.groupby('Order ID')['Product'].transform(lambda x : ','.join(x))
+        df = df[['Order ID','Grouped']]
+        for row in df['Grouped']:
+            row_list = row.split(',')
+            count.update(Counter(combinations(row_list, click3))) #update method will update results
+                                                             #2 is the combination of two products
 
-    a = st.dataframe(count.most_common(10))    #most_common will give top values
+        a = st.dataframe(count.most_common(10))    #most_common will give top values
+    if(click4):
+        st.dataframe(df[click4])
+except Exception as e:
+    st.title('This Operation is Not Possible.')
