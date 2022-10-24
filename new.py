86892c9b5f8ee7a9009c389bd724bd5d69b5b5dd
AbhiGithub101 <88555629+AbhiGithub101@users.noreply.github.com>
2022-10-24 11:27:04 +0530

    Add files via upload

--- new.py
+++ new.py
@@ -1,50 +1,38 @@
 import streamlit as st
-import pandas as pd
+import calendar
+from datetime import datetime
 
-df = pd.read_csv('Sales.csv')
-st.title('Console Flare - Sales Analysis : ')
-st.text('''In this project, we are going to analyze 12 months of data of sales. 
-We will learn how to clean, manage and analyze the dataset to 
-find some meaningful information.''')
-with st.sidebar:
-    click4 = st.multiselect('Show The Data', options=list(df.columns))
-    categorical = st.selectbox('Categorical', options=['Select']+list(df.columns))
-    numerical = st.selectbox('Numerical', options=['Select']+list(df.columns))
-    col1, col2, col3 = st.columns([1, 1, 1])
-    with col1:
-        click_sum = st.button('Sum')
-    with col2:
-        click_count = st.button('Count')
-    with col3:
-        pass
-    click3 = st.slider('Top Products Sold Together',0,5)
+page_title = 'Income Expense Tracker'
+page_icon = ":euro:"
+layout = "centered"
+st.set_page_config(page_title=page_title,page_icon=page_icon,layout="centered")
+st.title('Income Expense Tracker')
+year  = [datetime.today().year,datetime.today().year+1]
+month = list(calendar.month_name[1:])
+incomes = ['Salary','Blog','Other Income']
+expenses = ['Rent','Utilities','Saving','Groceries']
 
+st.subheader('Submission Form')
+with st.form('Data Entry Form',clear_on_submit=True):
+    col1,col2 = st.columns(2)
+    col1 = st.selectbox(label='Year',options=year,key='year')
+    col2 = st.selectbox(label='Month',options=month,key='month')
 
-try:
-    if (click_sum):
-        st.dataframe(df.groupby(categorical)[numerical].sum())
-        st.bar_chart(df.groupby(categorical)[numerical].sum())
-    elif(click_count):
-        st.dataframe(df.groupby(categorical)[numerical].count())
-        st.bar_chart(df.groupby(categorical)[numerical].count())
-    elif(click3>0):
-        all_data = df.dropna(how='all')
-        df = all_data[all_data['Order ID'].duplicated(keep=False)]
-
-        from itertools import combinations
-        from collections import Counter
-
-        count = Counter()
-        row_list = []
-        df['Grouped'] = df.groupby('Order ID')['Product'].transform(lambda x : ','.join(x))
-        df = df[['Order ID','Grouped']]
-        for row in df['Grouped']:
-            row_list = row.split(',')
-            count.update(Counter(combinations(row_list, click3))) #update method will update results
-                                                             #2 is the combination of two products
-
-        a = st.dataframe(count.most_common(10))    #most_common will give top values
-    if(click4):
-        st.dataframe(df[click4])
-except Exception as e:
-    st.title('This Operation is Not Possible.')
+    "---"
+    with st.expander(label="Income"):
+        for income in incomes:
+            st.number_input(label=income,format='%i',step=10,key=income)
+    with st.expander(label="Expense"):
+        for expense in expenses:
+            st.number_input(label=expense,format='%i',step=10,key=expense)
+    "---"
+    submitted = st.form_submit_button()
+    if submitted:
+        period = str(st.session_state['year']) + "_" + st.session_state['month']
+        incomes = {income : st.session_state[income] for income in incomes}
+        expenses = {expense : st.session_state[expense] for expense in expenses}
+    # to do - database
+    st.write(incomes)
+    st.write(expenses)
+    st.success('Data Saved')
+    
